72e8c1616262c3518f55208afaf0e2662ca4447a
Manon <chateaux@CERVVAL.com>
2025-11-28 09:16:57 +0100

    correct import error

--- api/main.py
+++ api/main.py
@@ -2,6 +2,22 @@ from fastapi import FastAPI, Request
 import pandas as pd
 import joblib
 
+class ModelWithThreshold:
+    """
+    Classe permettant d'encapsuler le modèle choisi avec le threshold optimal associé et donc d'utiliser directement la méthode predict et d'enregistrer le threshold au sein même du modèle sans devoir passer par deux fichiers séparés. 
+    """
+    def __init__(self, model, threshold):
+        self.model = model
+        self.threshold = threshold
+    
+    def predict(self, X):
+        proba = self.model.predict_proba(X)[:, 1]
+        return (proba >= self.threshold).astype(int)
+    
+    def predict_proba(self, X):
+        return self.model.predict_proba(X)
+
+
 # Charger le modèle au démarrage
 model = joblib.load("model_lightGBM.pkl")
 
