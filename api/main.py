3cb3da7f71733027b355784b284960409341f420
Manon <chateaux@CERVVAL.com>
2025-11-28 11:41:06 +0100

    try to solve model loading error

--- api/main.py
+++ api/main.py
@@ -3,21 +3,7 @@ import pandas as pd
 import joblib
 import pickle as pkl
 import os
-
-class ModelWithThreshold:
-    """
-    Classe permettant d'encapsuler le modèle choisi avec le threshold optimal associé et donc d'utiliser directement la méthode predict et d'enregistrer le threshold au sein même du modèle sans devoir passer par deux fichiers séparés. 
-    """
-    def __init__(self, model, threshold):
-        self.model = model
-        self.threshold = threshold
-    
-    def predict(self, X):
-        proba = self.model.predict_proba(X)[:, 1]
-        return (proba >= self.threshold).astype(int)
-    
-    def predict_proba(self, X):
-        return self.model.predict_proba(X)
+from api.models.custom_models import ModelWithThreshold
 
 
 # Charger le modèle au démarrage
